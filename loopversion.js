4715c4e60a21d46a3fa0074b8b56412af4e50563
soumya <79255255+soumyachikka@users.noreply.github.com>
2021-02-23 16:29:18 +0530

    loopversion

---
+++ loopversion.js
@@ -0,0 +1,12 @@
+// 5.2 looping version
+
+function loop(value, test, update, body) {
+  for (let i = value; test(i); update(i)) {
+    body(i)
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
