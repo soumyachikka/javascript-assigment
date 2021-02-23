518b33db36a28348b756413648ff993ba7a56202
soumya <79255255+soumyachikka@users.noreply.github.com>
2021-02-23 12:46:38 +0530

    Beancounting

---
+++ BeanCounting.js
@@ -0,0 +1,17 @@
+function countBs(s) {
+  var count = 0; 
+  for (var i = 0; i < s.length; i += 1) {
+    if (s.charAt(i) === "B")
+      count += 1;
+  }
+  return count;
+}
+
+function countChar(s, c) {
+  var count = 0; 
+  for (var i = 0; i < s.length; i += 1) {
+    if (s.charAt(i) === c)
+      count += 1;
+  }
+  return count;
+}
